b7769cffdf30578fb142c3befc7b89f5906f1c45
mii288 <atteeen@gmail.com>
2020-04-12 06:51:55 +0900

    chore: add setting for svg

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+const path = require('path')
+const withReactSvg = require('next-react-svg')
+
+let config = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/acnh_passport' : '',
 }
+
+config = withReactSvg({
+  include: path.resolve(__dirname, 'src/assets/images'),
+  webpack(config, _options) {
+    return config
+  }
+})
+
+module.exports = config
